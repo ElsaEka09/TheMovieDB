594b813443bec7cb9beb11cae4baa464abee31bc
hariskybagustio <1910031802060@sar.ac.id>
2023-06-09 09:36:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/themoviedb/MovieDetailActivity.java
@@ -0,0 +1,6 @@
+package com.example.themoviedb;
+
+import android.app.Activity;
+
+public class MovieDetailActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/themoviedb/MovieFragment.java
@@ -0,0 +1,6 @@
+package com.example.themoviedb;
+
+import android.app.Activity;
+
+public class MovieFragment extends Activity {
+}
